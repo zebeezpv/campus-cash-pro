418a74b00515d954d9725bb7a6c59c75621fd3c4
zebeezpv <deabreu0506@gmail.com>
2026-07-25 23:49:54 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE = 'campus-cash-pro-v6';
+const FILES = ['./','./index.html','./manifest.json'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE).map(key => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
